774eb065d3db74eb20438cfc3d376c8770fdd4c2
hcsilva <henriquec.dasilva@hotmail.com>
2020-07-14 22:38:10 -0300

    montando a estrutura inicial e footer com ion-slides

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,19 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+public options: Array<any> = [
+  {icon: 'person-add-outline', text: 'Indicar amigos'},
+  {icon: 'phone-portrait-sharp', text: 'Recarga de Celular'},
+  {icon: 'wallet-outline', text: 'Depositar'},
+  {icon: 'options-outline', text: 'Ajustar Limite'},
+  {icon: 'help-circle-outline', text: 'Me Ajuda'},
+  {icon: 'barcode-outline', text: 'Pagar'},
+  {icon: 'lock-open-outline', text: 'Bloquear Cartão'},
+  {icon: 'card-outline', text: 'Cartão Virtual'}
+];
+
+public slidesOptions: any = { slidesPerView: 2, freeMode: true};
+
   constructor() {}
 
 }
